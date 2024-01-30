8237f40ddc46273c0c37dbf98635363613307ee7
yayat_99 <rahmat999ht@gmail.com>
2024-01-30 08:40:05 +0800

    add assets

--- lib/constants/keys_assets.dart
+++ lib/constants/keys_assets.dart
@@ -6,4 +6,6 @@ class KeysAssets {
   static const String fresha = 'assets/images/fresha.svg';
   static const String sayur = 'assets/images/sayur.png';
   static const String empty = 'assets/images/empty.svg';
+  static const String bgLogin = 'assets/images/bgLogin.png';
+  static const String headerLogin = 'assets/images/headerLogin.png';
 }
